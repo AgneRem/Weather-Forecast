f8d233a630ec7af8ca7dcfcafea283ae2d062354
AgneRem <agne.remeikyte@gmail.com>
2018-03-27 16:41:44 +0300

    weather icon has been added

--- myScript.js
+++ myScript.js
@@ -25,7 +25,6 @@ $(document).ready(function(){
     fTemp = (kTemp*(9/5)-459.67).toFixed(1);
     cTemp = (kTemp - 273).toFixed(1);
 
-
     $("#city").html(city);
     $("#weatherType").html(weatherType);
     $("#fTemp").html(fTemp + " &#8457");
@@ -44,19 +43,45 @@ $(document).ready(function(){
 
     windSpeed = (2.237*(windSpeed)).toFixed(1);
     $("#windSpeed").html(windSpeed + " mph");
+
+    //checks what icon should be used:
+
+    var weatherIcons = {
+      "clear sky": "01",
+      "few clouds": "02",
+      "scattered clouds": "03",
+      "broken clouds": "04",
+      "shower rain": "09",
+      "rain": "10",
+      "thunderstorm": "11",
+      "snow": "13",
+      "mist": "50"
+    }
+
+    var iconToUse = weatherIcons[weatherType];
+
+    $("#weatherIcon").html("<img src=\"http://openweathermap.org/img/w/" + iconToUse + "d.png\" alt=\"\">");
+
+
     //checks the temperature and changes background-picture
     if(cTemp > 30){
-      $('body').css('background-image', 'url("https://images.unsplash.com/photo-1499479387933-4567e710809c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=f24dbbf141dd628c5bf767b03f438609&auto=format&fit=crop&w=334&q=80")');
-    } else if(cTemp > 20){
+     $('body').css('background-image', 'url("https://images.unsplash.com/photo-1499479387933-4567e710809c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=f24dbbf141dd628c5bf767b03f438609&auto=format&fit=crop&w=334&q=80")');
+    }
+    else if(cTemp > 20){
       $('body').css('background-image', 'url("https://d11in36igezwwb.cloudfront.net/texts/images/000/001/226/original/clouds.jpg?1507822094")');
-    } else if(cTemp > 10){
-      $('body').css('background-image', 'url("https://images.unsplash.com/photo-1521291668714-23cb608c52f1?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=e5eba68282357ff9b917fb2a363a7ffb&auto=format&fit=crop&w=334&q=80")');
+     } else if(cTemp > 10){
+       $('body').css('background-image', 'url("https://media.gettyimages.com/photos/sunrise-on-yosemite-valley-picture-id505872990?b=1&k=6&m=505872990&s=612x612&w=0&h=Aq3nxnL41EENnMdGNy6RkN78e-vPZfe_kO1WoEoYgtU=")');
       $('body').css("color", "white");
-    } else {
-      $('body').css('background-image', 'url("https://c.pxhere.com/photos/d6/89/clouds_cloudy_cold_daylight_fog_foggy_foliage_forest-911303.jpg!d")');
+     } else if(cTemp > 0) {
+      $('body').css("background-image", 'url("https://cdn.theatlantic.com/assets/media/img/mt/2017/06/GettyImages_675371680/lead_960.jpg?1498239007")');
+      $("body").css("color", "white");
      }
-   });
+   else {
+       $('body').css('background-image', 'url("https://c.pxhere.com/photos/d6/89/clouds_cloudy_cold_daylight_fog_foggy_foliage_forest-911303.jpg!d")');
+     }
   });
-}
+
+        });
+    }
 
 });
